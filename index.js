c623918b6e1c03ee227f5742417cf654cd25f3d5
eng-hussein-saad <eng.hussein.saad1@gmail.com>
2024-09-23 09:08:09 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,16 +11,7 @@ app.use(methodOverride("_method"));
 let post = [];
 let category = "Business";
 // const post = [...Array(9)]; //for copying each index of array and pasting it in the post array.
-post.push({
-  postTitle: "Manga",
-  day: "19",
-  month: "9",
-  year: "2024",
-  longDescription:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  id: "0",
-  category: "Business",
-});
+
 app.post("/submit", (req, res) => {
   const postTitle = req.body.postTitle;
   const category = req.body.category;
